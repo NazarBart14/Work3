b71e26e5e0ce81fa7c9907bdd3db5ce8cad4fdf2
Lenovo <Lenovo@DESKTOP-AJ5INH1>
2022-07-18 21:43:55 +0300

    Добавьте файлы проекта.

---
+++ Work3/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Text;
+using static System.Console;
+
+namespace Homework_2
+{
+    internal class Program
+    {
+
+        static void Main()
+        {
+            OutputEncoding = Encoding.Unicode;
+            Write("Write nomber 1 - ");
+            int odun = Convert.ToInt32(ReadLine());
+            Write("Write nomber 2 - "); ;
+            int dwa = Convert.ToInt32(ReadLine());
+            Write("Write nomber 3 - ");
+            int tru = Convert.ToInt32(ReadLine());
+            Write("Write nomber 4 - ");
+            int chot = Convert.ToInt32(ReadLine());
+
+            Console.WriteLine( odun + "" +  dwa + "" +   tru + "" +  chot + "");
+        }
+    }
+}
